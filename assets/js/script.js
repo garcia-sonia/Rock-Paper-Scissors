e4559e6c44585011f654a9719f3ec3b334f54f39
Sonia Garcia <soniagarciapascual@gmail.com>
2022-02-26 12:05:19 +0000

    Add buttons with images and try function with arrow in js to listen for the user click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
-const computerPicks = document.getElementById("computer-picks");
-const userPicks = document.getElementById("you-pick");
-const whoWins = document.getElementById("who-wins");
+const computerPicksShow = document.getElementById('computer-picks')
+const userPicksShow = document.getElementById('you-pick')
+const whoWinsShow = document.getElementById('who-wins')
+const differentChoices = document.querySelectorAll('button')
+
+let userPicks
+
+differentChoices.forEach(differentChoice => differentChoice.addEvent.Listener('click', (e) => {
+    userPicks = e.target.id
+    userPicksShow.innerHTML = userPicks
+}))
