c20416c494de9168355cf488fb7b0c78696cb276
Sonia Garcia <soniagarciapascual@gmail.com>
2022-03-05 11:46:07 +0000

    Create function to calculate and display result and add emoticons to result display

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,13 @@ const differentChoices = document.querySelectorAll('button')
 
 let youPick
 let computerPicks
+let result
 
 differentChoices.forEach(differentChoice => differentChoice.addEventListener('click', (e) => {
     youPick = e.target.id
     youPickShow.innerHTML = youPick
     generateComputerPicks()
+    calculateResult ()
 }))
 
 function generateComputerPicks () {
@@ -27,4 +29,37 @@ function generateComputerPicks () {
         computerPicks = 'scissors'
     }
     computerPicksShow.innerHTML = computerPicks
+}
+
+function calculateResult () {
+
+    if (computerPicks === youPick) {
+        result = 'It\'s a draw! &#128580;'
+    }
+
+    if (computerPicks === 'rock' && youPick === 'scissors') {
+        result = 'You lose! &#128531;'
+    }
+
+    if (computerPicks === 'rock' && youPick === 'paper') {
+        result = 'You win! &#128515'
+    }
+
+    if (computerPicks === 'paper' && youPick === 'scissors') {
+        result = 'You win! &#128515'
+    }
+
+    if (computerPicks === 'paper' && youPick === 'rock') {
+        result = 'You lose! &#128531;'
+    }
+
+    if (computerPicks === 'scissors' && youPick === 'rock') {
+        result = 'You win! &#128515'
+    }
+
+    if (computerPicks === 'scissors' && youPick === 'paper') {
+        result = 'You lose! &#128531;'
+    }
+
+    whoWinsShow.innerHTML = result
 }
