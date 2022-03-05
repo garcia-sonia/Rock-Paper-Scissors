02e37483a3746fcb998fd21070f3c0e3a0784b1b
Sonia Garcia <soniagarciapascual@gmail.com>
2022-03-05 11:22:32 +0000

    Create funtion for pirate choice of rock, paper, scissors

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,27 @@ const whoWinsShow = document.getElementById('who-wins')
 const differentChoices = document.querySelectorAll('button')
 
 let youPick
+let computerPicks
 
 differentChoices.forEach(differentChoice => differentChoice.addEventListener('click', (e) => {
     youPick = e.target.id
     youPickShow.innerHTML = youPick
+    generateComputerPicks()
 }))
+
+function generateComputerPicks () {
+    const anyNumber = Math.floor(Math.random () * differentChoices.length) + 1
+    
+    if (anyNumber === 1) {
+        computerPicks = 'rock'
+    }
+
+    if (anyNumber === 2) {
+        computerPicks = 'paper'
+    }
+
+    if (anyNumber === 3) {
+        computerPicks = 'scissors'
+    }
+    computerPicksShow.innerHTML = computerPicks
+}
