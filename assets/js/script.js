84fd5e37ff3201e9b9c8f0d55be7f70b84260a14
Sonia Garcia <soniagarciapascual@gmail.com>
2022-03-08 08:11:11 +0000

    Fix array of different choices and create const and function for modal box annoncing winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,35 @@
 const computerPicksShow = document.getElementById('computer-picks');
 const youPickShow = document.getElementById('you-pick');
 const whoWinsShow = document.getElementById('who-wins');
-const differentChoices = document.querySelectorAll('button');
+const differentChoices = document.getElementsByClassName('button');
 
-let youPick
-let computerPicks
-let result
+let youPick;
+let computerPicks;
+let result;
 
-differentChoices.forEach(differentChoice => differentChoice.addEventListener('click', (e) => {
-    youPick = e.target.id
-    youPickShow.innerHTML = youPick
-    generateComputerPicks()
-    calculateResult ()
+let scoreHTML = document.getElementById('user-score')[0];
+let computerHTML = document.getElementById('computer-score')[0];
+let userScore = 0;
+let computerScore = 0;
+
+
+Array.from(differentChoices).forEach(differentChoice => differentChoice.addEventListener('click', (e) => {
+    youPick = e.target.id;
+    youPickShow.innerHTML = youPick;
+    generateComputerPicks();
+    calculateResult();
 }))
 
-function generateComputerPicks () {
-    const anyNumber = Math.floor(Math.random () * differentChoices.length) + 1
-    
-    if (anyNumber === 1) {
-        computerPicks = 'rock'
-    }
+function generateComputerPicks() {
+    const anyNumber = Math.floor(Math.random() * 3) -1;
 
-    if (anyNumber === 2) {
-        computerPicks = 'paper'
-    }
-
-    if (anyNumber === 3) {
-        computerPicks = 'scissors'
-    }
-    computerPicksShow.innerHTML = computerPicks
+    const choices = ['rock', 'paper', 'scissors'];
+    computerPicks = choices[anyNumber] || 'rock';
+    computerPicksShow.innerHTML = computerPicks;
 }
 
 
-function win (youPick, computerPicks) {
-    userScore ++;
-    userScoreShow.innerHTML = userScore;
-    computerScoreShow.innerHTML = computerScore;
-}
-
-function lose (youPick, computerPicks) {
-    computerScore ++;
-    userScoreShow.innerHTML = userScore;
-    computerScoreShow.innerHTML = computerScore;
-}
-
-function calculateResult () {
+function calculateResult() {
 
     if (computerPicks === youPick) {
         result = 'It\'s a draw! &#128580;'
@@ -52,54 +37,64 @@ function calculateResult () {
 
     if (computerPicks === 'rock' && youPick === 'scissors') {
         result = 'You lose! &#128531;'
-        lose (youPick, computerPicks);
+        lose(computerHTML);
     }
 
     if (computerPicks === 'rock' && youPick === 'paper') {
         result = 'You win! &#128515'
-        win (youPick, computerPicks);
+        win(scoreHTML);
     }
 
     if (computerPicks === 'paper' && youPick === 'scissors') {
         result = 'You win! &#128515'
-        win (youPick, computerPicks);
+        win(scoreHTML);
     }
 
     if (computerPicks === 'paper' && youPick === 'rock') {
         result = 'You lose! &#128531;'
-        lose (youPick, computerPicks);
+        lose(computerHTML);
     }
 
     if (computerPicks === 'scissors' && youPick === 'rock') {
         result = 'You win! &#128515'
-        win (youPick, computerPicks);
+        win(scoreHTML);
     }
 
     if (computerPicks === 'scissors' && youPick === 'paper') {
         result = 'You lose! &#128531;'
-        lose (youPick, computerPicks);
+        lose(computerHTML);
     }
 
     whoWinsShow.innerHTML = result
 }
 
-let computerScore = 0;
-let userScore = 0;
+function win() {
+    userScoreShow.innerHTML = ++userScore;
+    winner();
+}
+
+function lose() {
+    computerScoreShow.innerHTML = ++computerScore;
+    winner();
+}
+
+
+
 let userScoreShow = document.getElementById('user-score');
 let computerScoreShow = document.getElementById('computer-score');
 let scoreBoardShow = document.querySelector('counter-display');
 
 /** Announce the winner using a Modal box (When a players scores 3 points the game ends and the winner is declared) */
 
-const endModal = getElementsByClassName('modal')[0];
-const modalHeader = getElementsByClassName('modal-header')[0];
-const modalContent = getElementsByClassName('modal-content')[0];
+const endModal = document.getElementsByClassName('modal')[0];
+const modalHeader = document.getElementsByClassName('modal-header')[0];
+const modalText = document.getElementsByClassName('modal-content')[0];
 
-function winner () {
+function winner() {
     if (userScore === 3) {
         endModal.style.display = 'block';
-        modalHeader.text.content = 'You won, matey!';
-        modalContent.text.content = `Your score: ${userScore} / Redbear's Score: ${computersScore}`;
+        modalHeader.textContent = 'You won, matey!';
+        modalText.textContent = `Your score: ${userScore} / Redbear's Score: ${computerScore}`;
     } else if (computerScore === 3) {
         endModal.style.display = 'block';
         modalHeader.text.content = 'You lost matey, but you can try again!';
