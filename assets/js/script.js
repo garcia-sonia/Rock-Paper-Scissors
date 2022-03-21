57daff916f9aca82cfc24585a30f50e418b69e56
Sonia Garcia <soniagarciapascual@gmail.com>
2022-03-21 08:29:31 +0000

    Add further comments to script.js file for a more structured and readable code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,19 @@ let youPick;
 let computerPicks;
 let result;
 
+let userScoreShow = document.getElementById('user-score');
+let computerScoreShow = document.getElementById('computer-score');
+
+//Default score values are set to 0 and incremented after each round until one of the players wins three rounds
 let userScoreHTML = document.getElementById('user-score')[0];
 let computerScoreHTML = document.getElementById('computer-score')[0];
 let userScore = 0;
 let computerScore = 0;
 
-
+//Add event listener on click for each choice of rock, paper or scissors from user.
+//Display user's choice for each round (innerHTML) 
+//Genearate computer's choice with generateComputerPicks() function.
+//Finally calculate result for each round with calculateResult() function.
 Array.from(differentChoices).forEach(differentChoice => differentChoice.addEventListener('click', (e) => {
     youPick = e.target.id;
     youPickShow.innerHTML = youPick;
@@ -21,6 +28,7 @@ Array.from(differentChoices).forEach(differentChoice => differentChoice.addEvent
     calculateResult();
 }))
 
+//Create function to generate computer choice with Math.random().
 function generateComputerPicks() {
     const anyNumber = Math.floor(Math.random() * differentChoices.length) + 1;
     const choices = ['rock', 'paper', 'scissors'];
@@ -28,7 +36,9 @@ function generateComputerPicks() {
     computerPicksShow.innerHTML = computerPicks;
 }
 
-
+//Create function to calculate result for each round taking every possible combination into account.
+//Display result (It's a draw, You lose! or You win!)
+//Apply lose() or win() function to increment scores on leaderboard depending on whether user/computer loses or wins.
 function calculateResult() {
 
     if (computerPicks === youPick) {
@@ -68,55 +78,38 @@ function calculateResult() {
     whoWinsShow.innerHTML = result
 }
 
+//Create function to increment user's score on leaderboard if user wins round.
+//Then show end of game modal (with Winner text) if user reaches a total score of three.
 function win() {
     userScoreShow.innerHTML = ++userScore;
     modalWinner();
 }
 
+//Create function to increment computer's score on leaderboard if user loses round.
+//Then show end of game modal (with Loser text) if computer reaches a total score of three.
 function lose() {
     computerScoreShow.innerHTML = ++computerScore;
     modalWinner();
 }
 
-let userScoreShow = document.getElementById('user-score');
-let computerScoreShow = document.getElementById('computer-score');
-
-
-
-/* Set up a button to open (on click) modal box with instructions on how to play the rock, paper, scissors game */
-//Declare About Button Variables - Game Instructions Pop Out (Modal) Box
+//Set up buttons to open and close (on click) modal box with Game Rules.
+//Declare Game Rules modal variables:
 const rulesBox = document.getElementsByClassName('rules-modal')[0];
 const rulesButton = document.getElementsByClassName('rules')[0];
 const closeSpan = document.getElementsByClassName('close')[0];
 
-// When user clicks the Game Rules Button, modal box with rules opens 
+//Add Event Listener: When user clicks the Game Rules Button, modal box with rules opens. 
 rulesButton.addEventListener('click', () => {
     rulesBox.style.display = 'block';
 });
 
-// When user clicks on (x), rules modal closes
+//Add Event Listner: When user clicks on (x), rules modal closes
 closeSpan.addEventListener('click', () => {
     rulesBox.style.display = 'none';
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/** Announce the winner using a Modal box (When a players scores 3 points the game ends and the winner is declared) */
-
+//Announce winner using an end of game modal box (When a player reaches a total score of three, the game ends and the winner is declared).
+//Declare end of game modal variables:
 const endModal = document.getElementsByClassName('end-game-modal')[0];
 const modalHeader = document.getElementsByClassName('end-modal-header')[0];
 const modalText = document.getElementsByClassName('end-modal-content')[0];
